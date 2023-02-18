c30465c9bba1f04706228237dc7d81795a9dffc9
Aavegjain <97734426+Aavegjain@users.noreply.github.com>
2023-02-18 23:42:24 +0530

    Delete helloworld.js

--- helloworld.js
+++
@@ -1,6 +0,0 @@
-const fs = require("fs") 
-
-fs.writeFileSync("notes.txt", "learning about file systems. ") 
-fs.writeFileSync("notes.txt", "learning some more") 
-
-fs.appendFileSync("notes.txt", " appending data") 
